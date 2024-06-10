492253c6a6b336d4ae3759546fd0b8a955492fa9
Gowthami Reddy <gowthamik710@gmail.com>
2024-06-10 17:14:39 +0530

    add : crud operations for user management system

---
+++ server/model/model.js
@@ -0,0 +1,22 @@
+// const { required } = require("joi");
+// const { type } = require("os");
+
+const mongoose = require('mongoose');
+
+let schema = new mongoose.Schema({
+    name : {
+        type : String,
+        required : true
+    },
+    email : {
+        type : String,
+        required : true,
+        unique : true
+    },
+    gender : String,
+    status : String
+})
+
+const Userdb = mongoose.model('userdb', schema);
+
+module.exports = Userdb;
